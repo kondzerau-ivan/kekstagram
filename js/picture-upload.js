ed037c5dfc86c7f6d6ca19151c95d8868b23e974
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2026-04-01 12:30:31 +0300

    6.9. Помощь друга (часть 1) - 3 пункт - Масштаб

--- js/picture-upload.js
+++ js/picture-upload.js
@@ -1,23 +1,45 @@
 import { isEscEvent } from './util.js';
 
 const body = document.querySelector('body');
-const inputFile = document.getElementById('upload-file');
 const imageOverlay = document.querySelector('.img-upload__overlay');
+const imageUploadPreview = imageOverlay.querySelector('.img-upload__preview img');
+const inputFile = document.getElementById('upload-file');
 const uploadCancel = document.getElementById('upload-cancel');
+const scaleControlSmaller = imageOverlay.querySelector('.scale__control--smaller');
+const scaleControl = imageOverlay.querySelector('.scale__control--value');
+const scaleControlBigger = imageOverlay.querySelector('.scale__control--bigger');
+const IMAGE_SCALE = {
+  'min': 25,
+  'max': 100,
+  'step': 25
+};
+let scaleControlValue = parseInt(scaleControl.value.substring(0, 3));
 
 const handleImageOverlayEscKeydown = (evt) => {
   if (isEscEvent(evt)) {
     evt.preventDefault();
     closeImageOverlay();
   }
-}
+};
 
-const closeImageOverlay = () => {
-  imageOverlay.classList.add('hidden');
-  body.classList.remove('modal-open');
-  inputFile.value = '';
+const handleScaleControlSmaller = () => {
+  if (scaleControlValue > IMAGE_SCALE.min) {
+    scaleControlValue -= IMAGE_SCALE.step;
+    scaleControl.value = `${scaleControlValue}%`;
+    scaleControl.dispatchEvent(new Event('change'));
+  }
+};
 
-  document.removeEventListener('keydown', handleImageOverlayEscKeydown);
+const handleScaleControlBigger = () => {
+  if (scaleControlValue < IMAGE_SCALE.max) {
+    scaleControlValue += IMAGE_SCALE.step;
+    scaleControl.value = `${scaleControlValue}%`;
+    scaleControl.dispatchEvent(new Event('change'));
+  }
+};
+
+const handleScaleControlValueChange = () => {
+  imageUploadPreview.style.transform = `scale(${scaleControlValue / 100})`;
 }
 
 inputFile.addEventListener('change', () => {
@@ -26,4 +48,19 @@ inputFile.addEventListener('change', () => {
 
   document.addEventListener('keydown', handleImageOverlayEscKeydown);
   uploadCancel.addEventListener('click', closeImageOverlay);
+  scaleControlSmaller.addEventListener('click', handleScaleControlSmaller);
+  scaleControlBigger.addEventListener('click', handleScaleControlBigger);
+  scaleControl.addEventListener('change', handleScaleControlValueChange);
 });
+
+const closeImageOverlay = () => {
+  imageOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  inputFile.value = '';
+
+  document.removeEventListener('keydown', handleImageOverlayEscKeydown);
+  uploadCancel.removeEventListener('click', closeImageOverlay);
+  scaleControlSmaller.removeEventListener('click', handleScaleControlSmaller);
+  scaleControlBigger.removeEventListener('click', handleScaleControlBigger);
+  scaleControl.removeEventListener('change', handleScaleControlValueChange);
+};
